a46c42460b49b456e6e8c5ae5cd072caddfbe319
acejang1994 <byumho.jang@students.olin.edu>
2015-02-17 13:57:24 -0500

    commiting to heroku

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(session({
 
 var PORT = process.env.PORT || 3000;
 var mongoURI = process.env.MONGOURI;
+mongoose.connect(mongoURI);
 
 app.get('/', index.login);
 app.get('/login', index.login);
@@ -35,8 +36,7 @@ app.post('/addingTwote', index.addTwote);
 app.post('/removingTwote', index.removeTwote);
 app.post('/logout', index.logout);
 
-mongoose.connect('mongodb://localhost/test');
-// mongoose.connect(mongoURI);
+// mongoose.connect('mongodb://localhost/test');
 
 app.listen(PORT, function() {
   console.log("Application running on port:", PORT);
